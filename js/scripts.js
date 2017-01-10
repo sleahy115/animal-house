06faeb2d0cff56a27dd3b8ac235823e57bcb01a5
David Quisenberry and Sarah Leahy <david@>
2017-01-10 13:21:46 -0800

    adds button to change background color

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  $("button#dark").click(function(){
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+  $("button#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("light-background");  
+  });
+});
